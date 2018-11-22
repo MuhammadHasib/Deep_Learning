c61c1b0c807d8264e938c3a9d3bdb7786cf26ccf
Muhammad Hasib <hasibdelhi@gmail.com>
2018-11-22 16:54:07 +0100

    Change KNN_Claddification.py

--- KNN_Classification.py
+++ KNN_Classification.py
@@ -27,3 +27,18 @@ classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 print(confusion_matrix(y_test, y_pred))  
 print(classification_report(y_test, y_pred))
+
+error = []
+
+# Calculating error for K values between 1 and 40
+for i in range(1, 40):  
+    knn = KNeighborsClassifier(n_neighbors=i)
+    knn.fit(X_train, y_train)
+    pred_i = knn.predict(X_test)
+    error.append(np.mean(pred_i != y_test))
+plt.figure(figsize=(12, 6))  
+plt.plot(range(1, 40), error, color='red', linestyle='dashed', marker='o',  
+         markerfacecolor='blue', markersize=10)
+plt.title('Error Rate K Value')  
+plt.xlabel('K Value')  
+plt.ylabel('Mean Error')
